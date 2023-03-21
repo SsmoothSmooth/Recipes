405da532d2e6eca839fd7990713acac322cadbc7
José Wilson de Lima <josewilson791@gmail.com>
2023-03-21 11:52:06 -0300

    :sparkles: feat: Event
    Aguardando o usuário clicar no botão 'Recipes' para abrir uma tabela com receitas

---
+++ recipes.js
@@ -0,0 +1,10 @@
+const btnRecipes = document.querySelector('.btn-Recipes');
+const typeRecipes = document.querySelector('.type-recipes')
+
+btnRecipes.addEventListener('click', function() {
+     
+   typeRecipes.style.visibility = "visible"; 
+
+  })
+
+  
